d75b5a2da80166f8cdf49cf8ee94922ed8c94cd4
Jan Bodnar <jan.bodnar@gmail.com>
2019-05-17 10:09:24 +0200

    Update and rename Mines.java to Minesweeper.java

--- src/com/zetcode/Minesweeper.java
+++ src/com/zetcode/Minesweeper.java
@@ -7,39 +7,40 @@ import javax.swing.JLabel;
 
 /**
  * Java Minesweeper Game
- * 
+ *
  * Author: Jan Bodnar
  * Website: http://zetcode.com
  */
 
-public class Mines extends JFrame {
+public class Minesweeper extends JFrame {
 
     private JLabel statusbar;
-    
-    public Mines() {
-        
+
+    public Minesweeper() {
+
         initUI();
     }
-    
+
     private void initUI() {
 
         statusbar = new JLabel("");
         add(statusbar, BorderLayout.SOUTH);
 
-        add(new Board(statusbar));        
-        
+        add(new Board(statusbar));
+
         setResizable(false);
         pack();
-        
+
         setTitle("Minesweeper");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
+
     public static void main(String[] args) {
-        
+
         EventQueue.invokeLater(() -> {
-            Mines ex = new Mines();
+
+            var ex = new Minesweeper();
             ex.setVisible(true);
         });
     }
